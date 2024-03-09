7afadde2d241ad23e5f158487bc3b780c57139bc
mouadim <mouadimami.yt@gmail.com>
2024-03-09 04:36:18 -0800

    all

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,27 +3,23 @@
 
 
 class Student:
-    """Represent a student."""
-
+    '''student class'''
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        '''initialization of the student object'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+    def tp_json(self, attrs=None):
+        '''return a derictory representation of student intence'''
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except  Exception:
+            return self.__dict__
+        my_dict = dict()
+        for key, value is self.__dict__.items():
+            if key is attrs:
+                my_dict(keyà = value
+        return my_dict
